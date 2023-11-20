7b4ca9c5b2c74d79b399a63746e3242ca2c23bf7
Ahmed Safwat <149934007+ASHmd97@users.noreply.github.com>
2023-11-20 14:46:03 +0200

    Update Text_Editor.py

--- Text_Editor.py
+++ Text_Editor.py
@@ -1,36 +1,4 @@
-'''
-Text Editor Project
-
-Introduction:
------------------
-This project is a simple text editor implemented using the Tkinter library in Python.
-The text editor provides basic functionality to open, edit, and save text files.
-It offers a user-friendly graphical interface with buttons for common operations such as opening a file and saving the edited content.
-
-Key Features:
------------------
-1.   -Open File-   | Allows the user to select and open a text file. The content of the file is displayed in the Text widget for editing.
-
-2.    -Save As-    | Enables the user to save the content of the Text widget to a new or existing text file. The user is prompted to choose a location for saving the file.
-
-3.  -Text Editing- | The main area of the application is a Text widget where users can input, edit, and view text content.
-
-Usage:
------------------
-1. Run the script, and a window will appear with a graphical user interface for the text editor.
-
-2. Use the "Open File" button to select and load the content of a text file into the text editor.
-
-3. Edit the text content using the Text widget.
-
-4. Use the "Save As" button to save the edited content to a new or existing text file.
-
-Dependencies:
------------------
-- Python 3.x
-- Tkinter library (included in standard Python distribution)
-
-'''
+# Text Editor Project
 
 import tkinter as tk
 from tkinter import *
